e003b944517cfd2d7eb96d4321c1939c9077d678
L4B0MB4 <metalfan65@gmail.com>
2018-11-07 21:37:47 +0100

    potential fixes

--- pi/Protocol.py
+++ pi/Protocol.py
@@ -3,17 +3,20 @@ import io
 class Protocol:
     isAlive = 0
     serialIO = None
+    ser = None
     lastRead = None
     lastReadString=""
-    def __init__(self,serialIO):
+    def __init__(self,serialIO,ser):
         self.serialIO = serialIO
+        self.ser = ser
         
     def sendAlive(self):
         self.serialIO.write(str("I am alive"))
         self.serialIO.flush() 
 
     def read(self):
-        self.lastRead = self.serialIO.read(5)
+        print("try read")
+        self.lastRead = self.ser.readline()
         print(self.lastRead)
         self.lastRead = self.serialIO.readline()
         self.lastReadString =str(self.lastRead,'utf-8').replace('\r', '')
--- pi/main.py
+++ pi/main.py
@@ -5,12 +5,13 @@ import Protocol
 
 ser = serial.Serial("/dev/ttyAMA0",9600)
 sio = io.TextIOWrapper(io.BufferedRWPair(ser, ser))
-protocol = Protocol.Protocol(sio)
-while True
+protocol = Protocol.Protocol(sio,ser)
+while True:
     #sio.write(str("hello\n"))
     #sio.flush()  
     #time.sleep(1)
     if not protocol.isAlive:
+        print("read")
         protocol.read()
     else:
         break
