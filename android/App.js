54db4a3aaa50b8c377716e091f521abb1091e7eb
L4B0MB4 <metalfan65@gmail.com>
2018-11-15 17:47:29 +0100

    Revert "probably not usable"
    This reverts commit 71f0becb02cd3093b1b0b41597c0427891927fbb.

--- android/App.js
+++ android/App.js
@@ -4,171 +4,102 @@
  *
  * @format
  * @flow*/
-
-import React, { Component } from "react";
-import { Platform, Text, View, DeviceEventEmitter, StyleSheet, AppRegistry, Button } from "react-native";
-import { PermissionsAndroid } from "react-native";
-import RNFetchBlob from "rn-fetch-blob";
-
-import Kontakt from "react-native-kontaktio";
+ 
+import React, { Component } from 'react';
+import { Platform, Text, View, DeviceEventEmitter, StyleSheet, AppRegistry } from 'react-native';
+import { PermissionsAndroid } from 'react-native';
+ 
+import Kontakt from 'react-native-kontaktio';
 const { connect, startScanning } = Kontakt;
-import { AudioRecorder, AudioUtils } from "react-native-audio";
-
+ 
 export default class MinimalExample extends Component {
+
   constructor(props) {
-    console.log(RNFetchBlob.fs.dirs);
     super(props);
-    this.state = {
-      beaconName: "",
-      beaconAddress: "",
-      beaconNamespace: "",
-      audioPath: RNFetchBlob.fs.dirs.DownloadDir + "/test.acc"
+     this.state = {
+      beaconName: '',
+      beaconAddress: '',
+      beaconNamespace: '',
     };
   }
-  prepareRecordingPath = audioPath => {
-    AudioRecorder.prepareRecordingAtPath(audioPath, {
-      SampleRate: 10,
-      Channels: 1,
-      AudioQuality: "Low",
-      AudioEncoding: "aac",
-      AudioEncodingBitRate: 1
-    });
-  };
 
-  async getPermission() {
-    try {
-      const granted = await PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION, {
-        title: "Location Permission",
-        message: "Beacon App needs access to your location " + "so you can locate Beacons."
-      });
-      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
-        console.log("You can use the location");
-      } else {
-        console.log("Location permission denied");
+  async getPermission(){
+     try {
+    const granted = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+      {
+        'title': 'Location Permission',
+        'message': 'Beacon App needs access to your location ' +
+                   'so you can locate Beacons.'
       }
-
-      connect(
-        undefined,
-        ["EDDYSTONE"]
-      )
-        .then(() => {
-          console.log("connect");
-          startScanning();
-        })
-        .catch(error => console.log("error", error));
-    } catch (err) {
-      console.warn(err);
+    )
+    if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+      console.log("You can use the location")
+    } else {
+      console.log("Location permission denied")
     }
+
+
+    connect(undefined,["EDDYSTONE"])
+      .then(() => {console.log("connect");startScanning()})
+      .catch(error => console.log('error', error));
+ 
+    
+  } catch (err) {
+    console.warn(err)
   }
+}
 
-  getPermissions = async () => {
-    let granted = await PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE, {
-      title: "storage write Permission",
-      message: "Beacon App needs access to your location " + "so you can locate Beacons."
-    });
-    if (granted === PermissionsAndroid.RESULTS.GRANTED) {
-      console.log("You can use the storage");
-    } else {
-      console.log("storage permission denied");
-    }
-    granted = await PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE, {
-      title: "storage read Permission",
-      message: "Beacon App needs access to your location " + "so you can locate Beacons."
-    });
-    if (granted === PermissionsAndroid.RESULTS.GRANTED) {
-      console.log("You can use the storage");
-    } else {
-      console.log("storage permission denied");
-    }
-    AudioRecorder.requestAuthorization().then(isAuthorised => {
-      this.setState({ hasPermission: isAuthorised });
-
-      if (!isAuthorised) return;
-
-      this.prepareRecordingPath(this.state.audioPath);
-
-      AudioRecorder.onProgress = data => {
-        console.log(data);
-        this.setState({ currentTime: Math.floor(data.currentTime) });
-      };
-
-      AudioRecorder.onFinished = data => {
-        console.log(data);
-      };
-    });
-  };
 
   componentDidMount() {
-    this.getPermissions();
+    DeviceEventEmitter.addListener(
+      'eddystoneDidAppear',
+      ({ eddystone,namespace}) => {
+        console.log('eddystoneDidAppear', eddystone,namespace);
+        this.setState({
+          beaconName: eddystone.name,
+          beaconAddress: eddystone.address,
+          beaconNamespace: eddystone.namespace
+        });
+      },
+    );
 
-    DeviceEventEmitter.addListener("eddystoneDidAppear", ({ eddystone, namespace }) => {
-      console.log("eddystoneDidAppear", eddystone, namespace);
-      this.setState({
-        beaconName: eddystone.name,
-        beaconAddress: eddystone.address,
-        beaconNamespace: eddystone.namespace
-      });
-    });
+    DeviceEventEmitter.addListener(
+      'eddystoneDidDisappear',
+      ({ eddystone,namespace}) => {
+        console.log('eddystoneDidDisappear', eddystone,namespace);
+        this.setState({
+          beaconName: '',
+          beaconAddress: '',
+          beaconNamespace: ''
+        });
+      },
+    );
 
-    DeviceEventEmitter.addListener("eddystoneDidDisappear", ({ eddystone, namespace }) => {
-      console.log("eddystoneDidDisappear", eddystone, namespace);
-      this.setState({
-        beaconName: "",
-        beaconAddress: "",
-        beaconNamespace: ""
-      });
-    });
+    DeviceEventEmitter.addListener(
+      'namespaceDidEnter',
+      ({ namespace}) => {
+        console.log('namespaceDidEnter',namespace);
+      },
+    );
+    
 
-    DeviceEventEmitter.addListener("namespaceDidEnter", ({ namespace }) => {
-      console.log("namespaceDidEnter", namespace);
-    });
-
-    DeviceEventEmitter.addListener("scanStatus", status => {
-      console.log("scanStatus", status);
-    });
-    this.getPermission()
-      .then(() => console.log("permissions"))
-      .catch(err => console.log(err));
+    DeviceEventEmitter.addListener(
+      'scanStatus',
+      (status) => {
+        console.log('scanStatus', status);
+      },
+    );
+    this.getPermission().then(()=>console.log("permissions")).catch((err)=>console.log(err))
   }
-
-  _record = async () => {
-    if (!this.state.hasPermission) {
-      console.warn("Can't record, no permission granted!");
-      return;
-    }
-
-    if (this.state.stoppedRecording) {
-      this.prepareRecordingPath(this.state.audioPath);
-    }
-
-    this.setState({ recording: true, paused: false });
-
-    try {
-      const filePath = await AudioRecorder.startRecording();
-    } catch (error) {
-      console.error(error);
-    }
-  };
-  _stop = async () => {
-    this.setState({ stoppedRecording: true, recording: false, paused: false });
-
-    try {
-      const filePath = await AudioRecorder.stopRecording();
-      console.log(filePath);
-      return filePath;
-    } catch (error) {
-      console.error(error);
-    }
-  };
-  pressRecord = () => {
-    this._record();
-    setTimeout(this._stop, 3000);
-  };
+ 
 
   render() {
-    const { beaconAddress, beaconName, beaconNamespace } = this.state;
-    if (beaconAddress == "C4:E7:AA:00:E5:7D" && beaconNamespace == "646f6f72426561636f6e") {
-      text = <Text>Dein Beacon "{beaconName}" ist in der Nähe. Die Tür wird jetzt geöffnet.</Text>;
+    const { beaconAddress, 
+      beaconName,
+      beaconNamespace } =  this.state;
+    if (beaconAddress == 'C4:E7:AA:00:E5:7D' && beaconNamespace == '646f6f72426561636f6e') {
+      text = <Text>Dein Beacon "{beaconName}" ist in der Nähe. Die Tür wird jetzt geöffnet.</Text>;    
     } else {
       text = <Text>Dein Beacon wurde nicht in der Nähe erkannt.</Text>;
     }
@@ -176,24 +107,23 @@ export default class MinimalExample extends Component {
       <View style={styles.container}>
         <Text style={styles.header}>Smarte Haustüröffnung</Text>
         <Text style={styles.text}>{text}</Text>
-        <Button onPress={this.pressRecord} title="Record" />
       </View>
-    );
+    ); 
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    backgroundColor: "#F5FCFF"
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
   },
   header: {
     fontSize: 25,
-    margin: 20
+    margin: 20,
   },
   text: {
     fontSize: 20,
-    margin: 10
-  }
+    margin: 10,
+  },
 });
