1eaddfb1a935e5b962411722468e65908270b7b2
L4B0MB4 <metalfan65@gmail.com>
2018-11-25 13:50:44 +0100

    added route led/on and led/off

--- pi/server.py
+++ pi/server.py
@@ -5,5 +5,14 @@ app = Flask(__name__)
 def hello():
     return "I bims ein Flask Server!"
 
+    
+@app.route("/led/on")
+def on():
+    return "An"
+    
+@app.route("/led/off")
+def off():
+    return "Aus"
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80, debug=True)
