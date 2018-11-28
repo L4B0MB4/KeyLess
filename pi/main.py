48956a7f8da4675a15ef6dd8c0e40740e6453f0d
L4B0MB4 <metalfan65@gmail.com>
2018-11-28 17:11:09 +0100

    added threading

--- pi/main.py
+++ pi/main.py
@@ -3,16 +3,15 @@ import io
 import serial
 from Protocol import protocolInstance
 from server import startServer
+from threading import Thread
 
 ser = serial.Serial("/dev/ttyAMA0", 9600)
 protocol = protocolInstance
 protocol.setSerial(ser)
-startServer()
-
+thread = Thread(target = startServer)
+thread.start()
+thread.join()
 while True:
-    # sio.write(str("hello\n"))
-    # sio.flush()
-    # time.sleep(1)
     if not protocol.isAlive:
         protocol.handleRead()
     else:
