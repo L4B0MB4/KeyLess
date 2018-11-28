a1bc68b0bd18003dabdf5387f1972a00299794b9
L4B0MB4 <metalfan65@gmail.com>
2018-11-28 17:31:17 +0100

    changed Thread

--- pi/main.py
+++ pi/main.py
@@ -5,14 +5,6 @@ from Protocol import protocolInstance
 from server import startServer
 from threading import Thread
 
-ser = serial.Serial("/dev/ttyAMA0", 9600)
-protocol = protocolInstance
-protocol.setSerial(ser)
-thread = Thread(target=readInput)
-thread.start()
-thread.join()
-startServer()
-
 
 def readInput():
     while True:
@@ -20,3 +12,12 @@ def readInput():
             protocol.handleRead()
         else:
             break
+
+
+ser = serial.Serial("/dev/ttyAMA0", 9600)
+protocol = protocolInstance
+protocol.setSerial(ser)
+thread = Thread(target=readInput)
+thread.start()
+thread.join()
+startServer()
