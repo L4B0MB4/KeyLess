1fbe58d797946574fc44cd0d98744df608b340b5
L4B0MB4 <metalfan65@gmail.com>
2019-01-03 00:17:13 +0100

    updated App.js and added new Components

---
+++ android/components/Beacon.js
@@ -0,0 +1,28 @@
+import { DeviceEventEmitter } from "react-native";
+import Kontakt from "react-native-kontaktio";
+const { connect, startScanning } = Kontakt;
+
+export async function startBeaconScanning(beaconAppeared, beaconDisappeared) {
+  const granted = await getPermission(PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION, "Location");
+  if (!granted) return;
+  initializeBeaconDetectors(beaconAppeared, beaconDisappeared);
+  await connect(
+    undefined,
+    ["EDDYSTONE"]
+  );
+  startScanning();
+}
+
+export function initializeBeaconDetectors(eddystoneDidAppear, eddystoneDidDisappear) {
+  DeviceEventEmitter.addListener("eddystoneDidAppear", ({ eddystone, namespace }) => {
+    eddystoneDidAppear(eddystone);
+  });
+
+  DeviceEventEmitter.addListener("eddystoneDidDisappear", ({ eddystone, namespace }) => {
+    eddystoneDidDisappear(eddystone);
+  });
+
+  DeviceEventEmitter.addListener("namespaceDidEnter", ({ namespace }) => {});
+
+  DeviceEventEmitter.addListener("scanStatus", status => {});
+}
