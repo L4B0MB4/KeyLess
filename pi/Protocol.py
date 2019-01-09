746a4ff6dd9e60989c4562e1b05a67c080813f7c
L4B0MB4 <metalfan65@gmail.com>
2019-01-09 18:30:17 +0100

    temp

--- pi/Protocol.py
+++ pi/Protocol.py
@@ -16,11 +16,6 @@ class Protocol:
         self.send("VERIFY-ALIVE")
 
     def send(self, value):
-        checksum = 0
-        for i in range(0, len(value)):
-            checksum += ord(value[i])
-        #print((str(value)+"||CHECKSUM||" + str(checksum)))
-        #self.ser.write((str(value)+"||CHECKSUM||" + str(checksum)).encode())
         self.ser.write((str(value)).encode())
 
     def read(self):
