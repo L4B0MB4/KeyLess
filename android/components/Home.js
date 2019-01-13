bbf7e5cdb1dcc3bb16157b0cbde8215150dd6518
Isabel <isabel.staaden@gmail.com>
2019-01-13 18:55:08 +0100

    merge fix

--- android/components/Home.js
+++ android/components/Home.js
@@ -1,62 +1,16 @@
-import { Text, View, StyleSheet, Picker } from "react-native";
-import { startBeaconScanning } from "./components/Beacon";
+import React, { Component } from "react";
+import { TouchableOpacity, Text, View, StyleSheet } from "react-native";
+import { Actions } from "react-native-router-flux";
+import { startBeaconScanning } from "./Beacon";
 
-export default class Beacon extends Component {
+export default class Home extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      beacon_list: [],
-      selected_beacon: ""
+      buttonPressed: "",
+      url: "192.168.0.107:8080"
     };
-
-    startBeaconScanning(this.eddystoneAppeared, this.eddystoneDisappeared, this.beaconAppeared, this.beaconDisappeared);
   }
-
-  eddystoneAppeared = eddystone => {
-    this.setState({
-      beacon_list: this.state.beacon_list.concat(eddystone)
-    });
-  };
-
-  eddystoneDisappeared = eddystone => {
-    const index = this.state.beacon_list.findIndex(list_beacon => this._isIdenticalEddystone(eddystone, list_beacon));
-    this.setState({
-      beacon_list: this.state.beacon_list.reduce((result, val, ind) => {
-        // don't add disappeared beacon to array
-        if (ind === index) return result;
-        // add all other beacons to array
-        else {
-          result.push(val);
-          return result;
-        }
-      }, [])
-    });
-  };
-
-  beaconAppeared = beacon => {
-    this.setState({
-      beacon_list: this.state.beacon_list.concat(beacon)
-    });
-  };
-
-  beaconDisappeared = beacon => {
-    const index = this.state.beacon_list.findIndex(list_beacon => this._isIdenticalBeacon(beacon, list_beacon));
-    this.setState({
-      beacon_list: this.state.beacon_list.reduce((result, val, ind) => {
-        // don't add disappeared beacon to array
-        if (ind === index) return result;
-        // add all other beacons to array
-        else {
-          result.push(val);
-          return result;
-        }
-      }, [])
-    });
-  };
-
-  _isIdenticalBeacon = (b1, b2) => b1.identifier === b2.identifier && b1.uuid === b2.uuid && b1.major === b2.major && b1.minor === b2.minor;
-
-  _isIdenticalEddystone = (b1, b2) => b1.identifier === b2.identifier && b1.name === b2.name && b1.namespace === b2.namespace && b1.instanceId === b2.instanceId;
 
   fetchThis = async route => {
     try {
@@ -66,40 +20,17 @@ export default class Beacon extends Component {
     }
   };
 
-  _renderBeacons = () => {
-    return (
-      <View>
-        <Picker onValueChange={itemValue => this.setState({ selected_beacon: itemValue })}>
-          {this.state.beacon_list.map(
-            (beacon, ind) => (
-              <Picker.Item label={beacon.proximity} value={beacon.proximity} />
-            ),
-            this
-          )}
-        </Picker>
-        <Text>Ausgewählter Beacon: {this.state.selected_beacon}</Text>
-      </View>
-    );
-  };
-
-  _renderEmpty = () => {
-    const { beacon_list } = this.state;
-    let text;
-    if (!beacon_list.length) text = "Keine Beacons in der Nähe";
-    return (
-      <View>
-        <Text>{text}</Text>
-      </View>
-    );
+  goToBeaconScreen = () => {
+    Actions.beacons();
   };
 
   render() {
-    const { beacon_list } = this.state;
     return (
       <View style={styles.container}>
         <Text style={styles.header}>Smarte Haustüröffnung</Text>
-        {beacon_list.length ? this._renderBeacons() : this._renderEmpty()}
-        <Text>{"\n Hot reload"}</Text>
+        <TouchableOpacity style={{ margin: 128 }} onPress={this.goToBeaconScreen}>
+          <Text>Go To Beacon Screen</Text>
+        </TouchableOpacity>
       </View>
     );
   }
