824dfdbb7566a9ee2edd4cd50c33223b9ee7d22a
L4B0MB4 <metalfan65@gmail.com>
2019-01-13 18:01:28 +0100

    trying old version

--- azure/index.js
+++ azure/index.js
@@ -1,70 +1,11 @@
-var port = process.env.PORT || 8080;
+var http = require("http");
 
-var express = require("express");
-var app = express();
-app.use(require("body-parser").json());
-
-var ownerCommands = [];
-var visitorRequests = [];
-
-app.get("/", function(req, res) {
-  res.send("welcome");
+var server = http.createServer(function(request, response) {
+  response.writeHead(200, { "Content-Type": "text/plain" });
+  response.end("Hello World!");
 });
 
-app.get("/azure ", function(req, res) {
-  res.send("welcome azure");
-});
-app.get("/azure/owner", function(req, res) {
-  if (visitorRequests.length > 0) {
-    res.send(visitorRequests);
-  } else {
-    res.send({ success: false });
-  }
-});
+var port = process.env.PORT || 1337;
+server.listen(port);
 
-/*
-  Format=> {
-	"command":"open-door",
-	"for":"beacon"
-  }
- */
-
-app.post("/azure/owner", function(req, res) {
-  var body = req.body;
-  var response = { sucess: false };
-  if (body.command === "open-door") {
-    ownerCommands.push(body);
-    response.sucess = true;
-  }
-  res.send(response);
-});
-
-app.get("/azure/visitor", function(req, res) {
-  if (ownerCommands.length > 0) {
-    res.send(ownerCommands);
-  } else {
-    res.send({ success: false });
-  }
-});
-
-/*
-  Format=> {
-	"request":"open-door",
-	"sender":"device e.g. android",
-	"beacon":"beacon-adress"
-  }
- */
-
-app.post("/azure/visitor", function(req, res) {
-  var body = req.body;
-  var response = { sucess: false };
-  if (body.request === "open-door") {
-    visitorRequests.push(body);
-    response.sucess = true;
-  }
-  res.send(response);
-});
-
-app.listen(port, function() {
-  console.log("Example app listening on port " + port + "!");
-});
+console.log("Server running at http://localhost:%d", port);
