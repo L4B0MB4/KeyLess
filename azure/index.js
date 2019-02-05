c9ab66bba2ae7172358e9be89ef7e02d6b016cb1
L4B0MB4 <metalfan65@gmail.com>
2019-02-05 20:02:25 +0100

    registering a device

--- azure/index.js
+++ azure/index.js
@@ -13,6 +13,14 @@ let dbClient = null;
 const ownerCommands = [];
 const visitorRequests = [];
 
+function createId() {
+  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function(c) {
+    var r = (Math.random() * 16) | 0,
+      v = c == "x" ? r : (r & 0x3) | 0x8;
+    return v.toString(16);
+  });
+}
+
 DB.connectMongoDB()
   .then(async function(client) {
     await DB.loadCommandsAndRequests(client, ownerCommands, visitorRequests);
@@ -27,6 +35,51 @@ DB.connectMongoDB()
       res.send("Not authenticated");
       next("Not authenticated");
     };
+
+    /*
+  Format=> {
+	"request":"open-door",
+	"sender":"device e.g. android",
+	"beacon":"beacon-adress"
+  }
+ */
+
+    app.post("/azure/visitor", async (req, res) => {
+      const body = req.body;
+      const response = { success: false };
+      if (body.request === "open-door") {
+        body.auth = req.query.auth;
+        const insertRes = await DB.insertInto(dbClient, "visitor", body);
+        visitorRequests.push(body);
+        response.success = insertRes.success === true;
+      }
+      res.send(response);
+    });
+
+    /*
+      Format=> {
+      "request":"register",
+      "device":"device e.g. android",
+      "beacon":"beacon-adress if beacon is device",
+      }
+    */
+    app.post("/azure/device", async (req, res) => {
+      const body = req.body;
+      const response = { success: false };
+      if (body.request === "register") {
+        if ((body.device === "beacon" && body.beacon) || (body.device !== "beacon" && !body.beacon && body.auth && req.query.auth > 8)) {
+          const device = {
+            id: createId(),
+            auth: req.query.auth,
+            beacon: body.beacon,
+            device: body.device
+          };
+          const insertRes = await DB.insertInto(dbClient, "devices", device);
+          response.success = insertRes.success === true;
+        }
+      }
+      res.send(response);
+    });
 
     app.use(authenticate);
 
@@ -65,26 +118,6 @@ DB.connectMongoDB()
       } else {
         res.send({ success: false });
       }
-    });
-
-    /*
-  Format=> {
-	"request":"open-door",
-	"sender":"device e.g. android",
-	"beacon":"beacon-adress"
-  }
- */
-
-    app.post("/azure/visitor", async (req, res) => {
-      const body = req.body;
-      const response = { success: false };
-      if (body.request === "open-door") {
-        body.auth = req.query.auth;
-        const insertRes = await DB.insertInto(dbClient, "visitor", body);
-        visitorRequests.push(body);
-        response.success = insertRes.success === true;
-      }
-      res.send(response);
     });
 
     dbClient = client;
