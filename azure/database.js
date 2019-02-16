b5e4031f8ccaa0ebd6494f8019df6f5812c59b2b
Lars <metalfan65@gmail.com>
2019-02-16 14:49:21 +0100

    creating device

---
+++ azure/database.js
@@ -0,0 +1,53 @@
+var mongoClient = require("mongodb").MongoClient;
+const DATABASE = "keyless";
+
+var url = process.env.MONGODB || "mongodb://localhost:27017";
+connectMongoDB = () => {
+  return new Promise(function(res, rej) {
+    mongoClient.connect(url, { useNewUrlParser: true }, function(err, client) {
+      if (err) rej(err);
+      console.log("Connected to MongoDB!");
+      res(client);
+    });
+  });
+};
+
+checkAuth = (client, auth, device) => {
+  return new Promise(async mainRes => {
+    var db = client.db(DATABASE);
+    var collection = db.collection("device");
+    const res = await collection.findOne({ auth, device });
+    mainRes(res);
+  });
+};
+
+insertInto = (client, collectionName, data) => {
+  return new Promise(function(mainRes, mainRej) {
+    var db = client.db(DATABASE);
+    data.db_timestamp = Date.now();
+    var collection = db.collection(collectionName);
+    collection.insertOne(data, function(err, res) {
+      if (err) {
+        mainRej(err);
+        return;
+      }
+      mainRes({ success: true });
+    });
+  });
+};
+
+loadCommandsAndRequests = async (client, owner, visitor) => {
+  const db = client.db(DATABASE);
+  let collection = db.collection("owner");
+  const resOwner = await collection.find().toArray();
+  collection = db.collection("visitor");
+  const resVisitor = await collection.find().toArray();
+  owner.push(...resOwner);
+  visitor.push(...resVisitor);
+};
+module.exports = {
+  connectMongoDB,
+  insertInto,
+  checkAuth,
+  loadCommandsAndRequests
+};
--- azure/index.js
+++ azure/index.js
@@ -1,46 +1,70 @@
-var port = process.env.PORT || 8080;
+require("@babel/core").transform("code", {
+  presets: ["@babel/preset-env"]
+});
 
-var express = require("express");
-var app = express();
+const port = process.env.PORT || 8080;
+
+const express = require("express");
+const app = express();
 app.use(require("body-parser").json());
+const DB = require("./database.js");
+let dbClient = null;
 
-var ownerCommands = [];
-var visitorRequests = [];
+const ownerCommands = [];
+const visitorRequests = [];
 
-app.get("/azure/owner", function(req, res) {
-  if (visitorRequests.length > 0) {
-    res.send(visitorRequests);
-  } else {
-    res.send({ success: false });
-  }
-});
+function createId() {
+  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function(c) {
+    var r = (Math.random() * 16) | 0,
+      v = c == "x" ? r : (r & 0x3) | 0x8;
+    return v.toString(16);
+  });
+}
 
-/*
-  Format=> {
-	"command":"open-door",
-	"for":"beacon"
-  }
- */
+DB.connectMongoDB()
+  .then(async function(client) {
+    await DB.loadCommandsAndRequests(client, ownerCommands, visitorRequests);
 
-app.post("/azure/owner", function(req, res) {
-  var body = req.body;
-  var response = { sucess: false };
-  if (body.command === "open-door") {
-    ownerCommands.push(body);
-    response.sucess = true;
-  }
-  res.send(response);
-});
+    /*
+      Format=> {
+      "request":"register",
+      "device":"device e.g. android",
+      "beacon":"beacon-adress if beacon is device",
+      }
+    */
+    app.post("/azure/device", async (req, res) => {
+      const body = req.body;
+      const response = { success: false };
+      if (body.request === "register") {
+        if (
+          (body.device === "beacon" && body.beacon) ||
+          (body.device !== "beacon" && !body.beacon)
+        ) {
+          const device = {
+            id: createId(),
+            auth: createId().toUpperCase(),
+            beacon: body.beacon,
+            device: body.device
+          };
+          const insertRes = await DB.insertInto(dbClient, "devices", device);
+          response.success = insertRes.success === true;
+        }
+      }
+      res.send(response);
+    });
 
-app.get("/azure/visitor", function(req, res) {
-  if (ownerCommands.length > 0) {
-    res.send(ownerCommands);
-  } else {
-    res.send({ success: false });
-  }
-});
+    authenticate = async (req, res, next) => {
+      if (req.query.auth) {
+        const res = DB.checkAuth(client, req.query.auth);
+        if (res) {
+          return next();
+        }
+      }
+      res.send("Not authenticated");
+      next("Not authenticated");
+    };
 
-/*
+    /*
   Format=> {
 	"request":"open-door",
 	"sender":"device e.g. android",
@@ -48,16 +72,69 @@ app.get("/azure/visitor", function(req, res) {
   }
  */
 
-app.post("/azure/visitor", function(req, res) {
-  var body = req.body;
-  var response = { sucess: false };
-  if (body.request === "open-door") {
-    visitorRequests.push(body);
-    response.sucess = true;
-  }
-  res.send(response);
-});
+    app.post("/azure/visitor", async (req, res) => {
+      const body = req.body;
+      const response = { success: false };
+      if (body.request === "open-door") {
+        body.auth = req.query.auth;
+        const insertRes = await DB.insertInto(dbClient, "visitor", body);
+        visitorRequests.push(body);
+        response.success = insertRes.success === true;
+      }
+      res.send(response);
+    });
 
-app.listen(port, function() {
-  console.log("Example app listening on port " + port + "!");
-});
+    app.use(authenticate);
+
+    app.get("/azure/owner", (req, res) => {
+      if (visitorRequests.length > 0) {
+        const foundRequests = visitorRequests.find(
+          item => item.auth == req.query.auth
+        );
+        res.send(foundRequests);
+      } else {
+        res.send({ success: false });
+      }
+    });
+
+    /*
+  Format=> {
+	"command":"open-door",
+  "for":"beacon"
+  }
+ */
+
+    app.post("/azure/owner", async (req, res) => {
+      const body = req.body;
+      const response = { success: false };
+      if (body.command === "open-door") {
+        body.auth = req.query.auth;
+        const insertRes = await DB.insertInto(dbClient, "owner", body);
+        ownerCommands.push(body);
+        response.success = insertRes.success == true;
+      }
+      res.send(response);
+    });
+
+    app.get("/azure/visitor", (req, res) => {
+      if (ownerCommands.length > 0) {
+        const foundCommands = ownerCommands.find(
+          item => item.auth == req.query.auth
+        );
+        res.send(foundCommands);
+      } else {
+        res.send({ success: false });
+      }
+    });
+
+    dbClient = client;
+    app.listen(port, function() {
+      console.log("Example app listening on port " + port + "!");
+    });
+    process.on("exit", function() {
+      client.close();
+    });
+  })
+  .catch(function(err) {
+    console.error("Error occured: ", err);
+  });
